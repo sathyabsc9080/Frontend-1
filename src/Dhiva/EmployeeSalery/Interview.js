9ae34bde1b9f7df17cea306d443fd6d2b1978f9b
kannanyaazhetech <apachekannan948@gmail.com>
2025-03-17 14:42:33 +0530

    Update Interview.js

--- src/Dhiva/EmployeeSalery/Interview.js
+++ src/Dhiva/EmployeeSalery/Interview.js
@@ -1,48 +1,35 @@
-// We have a list of products available in our Levi's showroom.
-
-// There are 5 orders to be executed for a particular date.
-
-// After executing these orders, you should display the final total sales amount for the day.
-
-// Note: Each order bill should include 18% GST.
- 
-
 import React from 'react'
 
 const Interview = () => {   
+    const products = [
+        { name: 'Shirt', price: 100, quantity: 5 },
+        { name: 'Pant', price: 200, quantity: 3 },
+        { name: 'Tshirt', price: 150, quantity: 7 },
+    ];
 
+    const calculation = (price , quantity) => {
+      const Sale = price * quantity;
+      const gst = Sale * 0.18 ;
+      return Sale + gst;
+    }
 
-  //   const products = [
-  //     { name: 'Shirt', price: 100, quantity: 5 },
-  //     { name: 'Pant', price: 200, quantity: 3 },
-  //     { name: 'Tshirt', price: 150, quantity: 7 },
-  //   ]
-    
-  //  const calculation = (price , quantity) => {
-  //   const totalSales = price * quantity
-  //   const totalGST = totalSales * 0.18
-  //   const totalSaleAmount = totalSales + totalGST;
-  //   return totalSaleAmount;
-    
-  //  }
-    
-  //   return (
-  //     <div>
-  //       {products.map((product) => (
-  //           <>
-  //           <h2>Calculation:</h2>
-  //         <div key={product.name}>
-  //           <h2>{product.name}</h2>
-  //           <p>Price: {product.price}</p>
-  //           <p>Quantity: {product.quantity}</p>
-  //           <p>Total Sale Amount: {calculation(product.price, product.quantity)}</p>
-  //         </div>
-  //         </>
-  //       ))}
-  //       <h2>Total Sales Amount: {products.map(calculation).reduce((a, b) => a + b)}</h2>
-  //     </div>
-  //   )
-    
-  
-}
-export default Interview
+    const totalSalesAmount = products
+        .map(products => calculation(products.price , products.quantity))
+        .reduce((a,b)=>(a+b),0)
+    return (
+        <div>
+            <h2>Our Details</h2>
+            {products.map((product)=>(
+              <div key={product.name}>
+                  <h3>Product : {product.name}</h3>
+                  <p>Price : {product.price}</p>
+                  <p>Quantity : {product.quantity}</p>
+                  <p>Total Sale Amount (GST) : {calculation(product.price , product.quantity)}</p>
+              </div>
+            ))}
+            <h3>Total Sale Amount Of The Day:{totalSalesAmount}</h3>
+        </div>
+    );
+};
+
+export default Interview;
