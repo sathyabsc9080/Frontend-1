e8d30206e26ca3468e551d28c1e421ccf4374b3a
Rupa2298 <138680466+Rupa2298@users.noreply.github.com>
2025-06-13 11:34:17 +0530

    mapping

---
+++ src/Mapping/Maping.jsx
@@ -0,0 +1,4 @@
+/*Frontend [ui]->   user ->    passing request ->  Api ->   backend -endpoint triggering ->  
+  fetching data from Database ->   backend service ->   response ->   user - UI\
+  
+*/
