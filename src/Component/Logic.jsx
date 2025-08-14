e67c298a6991062f6af2303dfdd7df5c0d18179f
kanakarasu20@gmail.com <sathya.palanisami@outlook.com>
2025-08-14 11:37:51 +0530

    logic.jsx

--- src/Component/Logic.jsx
+++ src/Component/Logic.jsx
@@ -29,22 +29,50 @@ const Logic = () => {
 
  
   return (
-    <div>
-      <h3>User Data</h3>
-      {loading && <div>Loading...</div>}
-      {error && <div style={{ color: 'red' }}>Error: {error}</div>}
-      {data && !error && Array.isArray(data) && (
-        <div>
-          <ul>
-            {data.map(user => (
-              <li key={user.id}>
-                <strong>{user.name}</strong> - {user.email} - {user.address.city}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+   <div>
+  <h3  style={{ fontSize:"30px",color:"blue" , }}>User Data</h3>
+
+  {loading && <div>Loading...</div>}
+  {error && <div style={{ color: 'red' }}>Error: {error}</div>}
+
+  {data && !error && Array.isArray(data) && (
+    <table border="1" cellPadding="8" style={{ borderCollapse: 'collapse', width: '100%' }}>
+      <thead>
+        <tr >
+          <th style={{ fontSize:"20px",color:"red" , }}>ID</th>
+          <th style={{ fontSize:"20px",color:"red" , }}>Name</th>
+          <th style={{ fontSize:"20px",color:"red" , }}>Username</th>
+          <th style={{ fontSize:"20px",color:"red" , }}>Email</th>
+          <th style={{ fontSize:"20px",color:"red" , }}>Address</th>
+          <th style={{ fontSize:"20px",color:"red" , }}>Phone</th>
+          <th style={{ fontSize:"20px",color:"red" , }}>Website</th>
+          <th style={{ fontSize:"20px",color:"red" , }}>Company</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(user => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+            <td>
+            <strong>Street:</strong>{user.address.street} -
+            <strong>Suite:</strong>{user.address.suite} -
+            <strong>City:</strong>{user.address.city} -
+            <strong>Zipcode:</strong>{user.address.zipcode} 
+            </td>
+            <td> <strong>Phone:</strong>{user.phone}</td>
+            <td> <strong>WebSite:</strong>{user.website}</td>
+            <td> <strong>Name:</strong>{user.company.name} -
+                  <strong>bs:</strong>{user.company.bs}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )}
+</div>
   );
 };
 
